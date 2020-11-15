7468c958f64b5c1bd112ed8bdc573f4e0a4ecd56
Kery Long <71454410+KeryLong@users.noreply.github.com>
2020-11-15 11:06:30 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function addTeamMembers(){
+  let firstName = document.createElement("input");
+let appendDiv = document.getElementById("addTeamMemberDiv");
+  firstName
+  firstName.appendChild(appendDiv);
+ 
+  document.getElementById("addTeamMemberDiv").appendChild(firstName);     
+
+}
